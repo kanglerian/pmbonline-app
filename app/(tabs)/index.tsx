71b01ca2a84bb31f0112f2b3c8c35e8f446b2167
Lerian Febriana <kanglerian@gmail.com>
2024-07-27 14:45:14 +0700

    first commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,70 +1,52 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
+import React, { useState } from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import axios from 'axios';
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+const index = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [name, setName] = useState('loading...');
 
-export default function HomeScreen() {
+  const handleLogin = async () => {
+    await axios.post(`http://103.163.111.39:3106/auth/login`,{
+      email: email,
+      password: password
+    })
+    .then((response) => {
+      console.log(response.data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+    
+  }
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
-  );
+    <SafeAreaView>
+      <View className="flex flex-col items-center justify-center h-screen bg-gray-50">
+        <View className="w-full max-w-lg mx-auto px-5 flex flex-col items-center p-6 border border-gray-200 rounded-3xl">
+          <View>
+            <Text className="text-center font-bold text-2xl">PMB Online Apps</Text>
+            <Text className="text-center">{name}</Text>
+            <Text className="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, autem.</Text>
+          </View>
+          <View className="w-full space-y-2">
+            <View className="mt-5">
+              <TextInput placeholder='Email' value={email} onChangeText={setEmail} keyboardType='email-address' autoCapitalize='none' className="w-full bg-gray-200 border border-gray-300 rounded-2xl p-2.5 px-4" />
+            </View>
+            <View className="mt-5">
+              <TextInput placeholder='Password' value={password} onChangeText={setPassword} autoCapitalize='none' secureTextEntry={true} className="w-full bg-gray-200 border border-gray-300 rounded-2xl p-2.5 px-4" />
+            </View>
+            <TouchableOpacity onPress={handleLogin} className="bg-sky-500 p-3 rounded-2xl" activeOpacity={0.8}>
+              <Text className="text-center text-white font-medium">Login</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    </SafeAreaView>
+  )
 }
 
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
+export default index
+
+const styles = StyleSheet.create({})
